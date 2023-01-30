6adc5389785422f5f20f79f0083cf5178623c144
Pandidurai.S <100015536+Pandidurai-22@users.noreply.github.com>
2023-01-30 11:21:37 +0530

    Create facedetection.py

---
+++ AI/FaceDetection/facedetection.py
@@ -0,0 +1,20 @@
+import cv2
+
+algo = "haarcascade_frontalface_default.xml"
+haar_Cascade = cv2.CascadeClassifier(algo)
+
+cam = cv2.VideoCapture(0)
+
+while True :
+    _,img = cam.read()
+    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face = haar_Cascade.detectMultiScale(grayImg,1.3,4)
+    for (x,y,w,h) in face :
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("faceDetection",img)
+    key = cv2.waitkey(10)
+    if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
+
